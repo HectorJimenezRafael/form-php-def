f262f5cfac082c8f2e9dfd8bd41e509651c3ea6b
Hector <10000530.joan23@fje.edu>
2022-02-09 12:40:10 +0100

    first commit

--- proc/res.proc.php
+++ proc/res.proc.php
@@ -1,12 +1,19 @@
 <?php
 // index -> pantalla1
 if (isset($_POST['index']) && strtolower($_POST['pokemon'])=='pikachu') {
+    // creo la sesión por primera vez.
+    session_start();
+    $_SESSION['pantalla1']='check';
+
     header('Location: ../view/pantalla1.php');
 } else if(isset($_POST['index'])){
     header('Location: ../index.php?msg=25');
 }
 // pantalla1 -> pantalla2
 else if(isset($_POST['pantalla1']) && strtolower($_POST['pokemon'])=='charmander'){
+    // sesión para la pantalla2, recuperar sesión
+    session_start();
+    $_SESSION['pantalla2']='check';
     header('Location: ../view/pantalla2.php');
 } else if (isset($_POST['pantalla1'])){
     header('Location: ../view/pantalla1.php?msg=4');
